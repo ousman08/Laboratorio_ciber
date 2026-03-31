067056892929e43864a9d14acfbef0b315f8cd34
Ousman <oman7910@gmail.com>
2026-03-31 23:22:06 +0200

    add greeting function and average calculation exercise

--- ProgrmacionPython/6.Ejercicios.py
+++ ProgrmacionPython/6.Ejercicios.py
@@ -3,7 +3,7 @@
 # 2. Crea una función que reciba una lista de números y devuelva el promedio de
 # esos números.
 
-
+"""
 print("1. Escribe una función que reciba un nombre y devuelva un saludo personalizado.")
 
 def saludo(nombre):
@@ -19,7 +19,20 @@ mensaje = saludo(usuario)
 
 #3. Mostramos el resultado
 print(mensaje)
-
-
+"""
 
 print("2. Crea una función que reciba una lista de números y devuelva el promedio de esos números.")
+
+
+def promedio(lista):
+    total = 0 #inicializamos una variable para acumular la suma de todos los numeros
+    
+    #Recorremos cada numero de la lista
+    for num in lista:
+        total += num #sumamos cada numero al total
+
+    #calculamos la suma total dividiendo lo con la cantidad de numeros en la lista
+    return total / len(lista)
+    
+lista = [12, 12]
+print(promedio(lista))

ad2b45ce4a7c999c4beb14e062d82fcec4ed30f4
Ousman <oman7910@gmail.com>
2026-04-01 00:17:22 +0200

    testing

--- ProgrmacionPython/8.Ejercicios.py
+++ ProgrmacionPython/8.Ejercicios.py
@@ -2,3 +2,5 @@
 # 1. Usa el módulo datetime para mostrar la hora exacta de ejecución del script.
 # 2. Utiliza re para detectar y extraer todas las IPs presentes en un texto simulado de
 # # log.
+
+print("1. Usa el módulo datetime para mostrar la hora exacta de ejecución del script.")

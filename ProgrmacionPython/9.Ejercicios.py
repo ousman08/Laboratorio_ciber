83f4400d5a03e5b7fa19871eca5696f38ae8ab67
Ousman <oman7910@gmail.com>
2026-04-01 00:08:08 +0200

    Add input validation with try/except for numeric input

--- ProgrmacionPython/9.Ejercicios.py
+++ ProgrmacionPython/9.Ejercicios.py
@@ -3,3 +3,15 @@
 # texto.
 # 2. Crea un script que intente abrir un archivo que no existe y maneje el error
 # mostrando un mensaje personalizado.
+
+
+print("1. Escribe un programa que solicite un número y gestione el error si se ingresa texto.")
+try:
+   num = int(input("Escribe un numero: "))
+   print(f"Tu numero es: {num}")
+   
+except ValueError:
+    print("Error, no has escrito un numero")
+    
+    
+print("2. Crea un script que intente abrir un archivo que no existe y maneje el error mostrando un mensaje personalizado.")
